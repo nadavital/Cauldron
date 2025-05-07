75ae280b3d33522b04ca81a37e937def36a40270
Nadav Avital <nadav.avital@icloud.com>
2025-05-06 20:38:54 -0700

    Initial Commit

---
+++ Cauldron/CauldronApp.swift
@@ -0,0 +1,17 @@
+//
+//  CauldronApp.swift
+//  Cauldron
+//
+//  Created by Nadav Avital on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CauldronApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
